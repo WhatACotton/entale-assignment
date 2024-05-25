9b201814168e1df1cffd68ba0119ef3e544d93ea
WhatACotton <whatacotton@gmail.com>
2024-05-25 23:33:15 +0900

    chore: fix dependency

--- go.mod
+++ go.mod
@@ -3,10 +3,8 @@ module github.com/whatacotton/entale-assignment
 go 1.22.3
 
 require (
-	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/go-chi/chi v1.5.5 // indirect
-	github.com/go-chi/chi/v5 v5.0.12 // indirect
-	github.com/go-sql-driver/mysql v1.8.1 // indirect
-	github.com/jmoiron/sqlx v1.4.0 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/go-chi/chi v1.5.5
+	github.com/go-sql-driver/mysql v1.8.1
 )
+
+require filippo.io/edwards25519 v1.1.0 // indirect
--- internal/repository.go
+++ internal/repository.go
@@ -1,6 +1,11 @@
 package internal
 
-import "database/sql"
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func RegisterArticleToRepoitory(db *sql.DB, article Article) error {
 	tx, _ := db.Begin()
@@ -66,4 +71,49 @@ ON
 	}
 
 	return articles, nil
+}
+
+const articleInit = `
+CREATE TABLE IF NOT EXISTS articles (
+	id int(11) NOT NULL,
+	title text NOT NULL,
+	body text NOT NULL,
+	published_at text NOT NULL,
+	PRIMARY KEY (id)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+`
+
+const mediaInit = `
+CREATE TABLE IF NOT EXISTS medias (
+	id int(11) NOT NULL,
+	article_id int(11) NOT NULL,
+	content_url text NOT NULL,
+	content_type varchar(100) NOT NULL,
+	PRIMARY KEY (id),
+	KEY medias_articles_FK (article_id),
+	CONSTRAINT medias_articles_FK FOREIGN KEY (article_id) REFERENCES articles (id)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+`
+
+func ConnectSQL() (db *sql.DB) {
+	// データベースのハンドルを取得する
+	db, err := sql.Open("mysql", "root:password@tcp(entaleAssignmentdb:3306)/entaleAssignment?parseTime=true")
+
+	if err != nil {
+		// ここではエラーを返さない
+		log.Fatal(err)
+	}
+	return db
+}
+
+func DBInit(db *sql.DB) error {
+	_, err := db.Exec(articleInit)
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec(mediaInit)
+	if err != nil {
+		return err
+	}
+	return nil
 }
